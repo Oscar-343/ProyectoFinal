8bace9049823e3d9fbe48246c6065941beb68c92
oscarrodrigorivassossa70@gmail.com <oscarrodrigorivassossa70@gmail.com>
2026-08-23 16:33:42 -0400

    Conexion a Base de Datos

---
+++ ProyectoFinal/DB/DBcontext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Data
+{
+    public class TiendaDbContext : DbContext
+    {
+        public TiendaDbContext(DbContextOptions<TiendaDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<material> Material { get; set; }
+        public DbSet<modelo> Modelo { get; set; }
+        public DbSet<modelo_material> Modelo_Material { get; set; }
+        public DbSet<usuario> Usuarios { get; set; }
+        // Agreguen aquí las entidades que falten
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Aquí van configuraciones extra
+        }
+    }
+}
--- ProyectoFinal/Program.cs
+++ ProyectoFinal/Program.cs
@@ -1,29 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ProyectoFinal.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<TiendaDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-
-app.Run();
+//resto del pipeline
